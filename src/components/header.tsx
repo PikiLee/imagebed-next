fb56d226f9513fc4a210dd02473b520cc43d1e61
PikiLee <871565198@qq.com>
2024-02-24 18:07:54 +0800

    feat: Add navigation links to header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,17 @@
 import { signOut } from 'next-auth/react'
 import { H1 } from './ui/h1'
 import { Button } from './ui/button'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 flex items-center justify-between w-full h-16 px-8 bg-white">
       <H1>Image Bed Next</H1>
-      <Button onClick={() => signOut()}>Sign Out</Button>
+      <nav className="flex items-center gap-4">
+        <Link href="/">Upload</Link>
+        <Link href="/history">History</Link>
+        <Button onClick={() => signOut()}>Sign Out</Button>
+      </nav>
     </header>
   )
 }
