9b142a2c817e9e00c3ae275827c42d6f66e10d7e
PikiLee <871565198@qq.com>
2024-02-25 20:23:00 +0800

    refactor: key.ts to remove environment prefix

--- src/lib/key.ts
+++ src/lib/key.ts
@@ -4,9 +4,7 @@ export function generateID() {
   return randomUUID()
 }
 
-// Add a prefix to the key to separate dev and prod files
-const environmentPrefix = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
-export const prefix = `${environmentPrefix}/images/`
+export const prefix = `/images/`
 
 export function getImageKey(id: string) {
   return `${prefix}${id}`
