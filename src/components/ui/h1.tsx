4223e701ec76c3a177b3a1e8d1b0169c7580fd1e
PikiLee <871565198@qq.com>
2024-02-26 12:01:32 +0800

    refactor: font size in H1 component

--- src/components/ui/h1.tsx
+++ src/components/ui/h1.tsx
@@ -1,6 +1,6 @@
 export function H1({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">
       {children}
     </h1>
   )
