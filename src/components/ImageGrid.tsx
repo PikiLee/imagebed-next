e50b9b790d3f77751a239a5ba605f135beb6b4f1
PikiLee <871565198@qq.com>
2024-02-26 11:55:08 +0800

    refactor: Update ImageGrid layout to be responsive

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -46,7 +46,7 @@ export default function ImageGrid({}: {}) {
 
   return (
     <>
-      <ul className="grid grid-cols-3 gap-4">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {urls?.map((url) => {
           return (
             url && (
