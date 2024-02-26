e290722a760e3ee3ca7785a2a5daf94af0454554
PikiLee <871565198@qq.com>
2024-02-26 11:38:44 +0800

    refactor: Remove heading from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { H2 } from '@/components/ui/h2'
 import UploadButton from '@/components/upload-button/upload-button'
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-start gap-12">
-      <H2>Upload Image</H2>
       <UploadButton />
     </main>
   )
