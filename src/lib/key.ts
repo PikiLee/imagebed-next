3074deb2d40685f7cfedff8445aff9141889ec71
PikiLee <871565198@qq.com>
2024-02-26 18:44:16 +0800

    feat: Add reversed timestamp to generateID function

--- src/lib/key.ts
+++ src/lib/key.ts
@@ -4,7 +4,12 @@ import { isDev } from './isDev'
 import { publicEnv } from './publicEnv'
 
 export function generateID() {
-  return randomUUID()
+  // reversed timestamp is used to sort images descending by creation date
+  const futureDate = new Date('9999-12-31T23:59:59.999Z').getTime()
+  const now = Date.now()
+  const reversedTimestamp = futureDate - now
+
+  return `${reversedTimestamp}-${randomUUID()}`
 }
 
 export const prefix = `images/`
