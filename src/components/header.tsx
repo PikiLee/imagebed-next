e17c4b4071c31aec7b4d4cb58f0122931260873f
PikiLee <871565198@qq.com>
2024-02-26 21:43:09 +0800

    feat: Update header to include a link to the home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,9 @@ import { H1 } from './ui/h1'
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 flex items-center justify-between w-full h-16 px-8 bg-white">
-      <H1>Image Bed Next</H1>
+      <Link href="/">
+        <H1>Image Bed Next</H1>
+      </Link>
       <nav className="flex items-center gap-4">
         <Link href="/">Upload</Link>
         <Link href="/history">History</Link>
