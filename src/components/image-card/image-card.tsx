3d0d34984224b11fe7f4112874bb15eeb70b5475
PikiLee <871565198@qq.com>
2024-03-10 14:30:36 +0800

    refactor: image card component to use icons

--- src/components/image-card/image-card.tsx
+++ src/components/image-card/image-card.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Link from 'next/link'
+import { Copy, Trash2 } from 'lucide-react'
 import { ComponentProps } from 'react'
 
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
@@ -22,28 +22,31 @@ export default function ImageCard({
     <Card {...props}>
       <CardHeader></CardHeader>
       <CardContent>
-        <img src={url} alt="Uploaded Image" width={1920} height={1080} />
-      </CardContent>
-      <CardFooter>
-        <div className="flex flex-col gap-4">
-          <Link href={url} target="_blank" className="break-all">
-            {url}
-          </Link>
-          <Button
-            onClick={() => {
-              navigator.clipboard.writeText(url)
-              toast({
-                title: 'URL copied',
-              })
-            }}
-          >
-            Copy URL
-          </Button>
-          <Button onClick={() => onDelete(url)} variant="destructive">
-            Delete
-          </Button>
+        <div className="relative">
+          <img src={url} alt="Uploaded Image" width={1920} height={1080} />
+          <div className="absolute top-4 right-4 flex gap-2">
+            <Button
+              onClick={() => {
+                navigator.clipboard.writeText(url)
+                toast({
+                  title: 'URL copied',
+                })
+              }}
+              aria-label="Copy URL"
+            >
+              <Copy size={16} />
+            </Button>
+            <Button
+              onClick={() => onDelete(url)}
+              variant="destructive"
+              aria-label="Delete"
+            >
+              <Trash2 size={16} />
+            </Button>
+          </div>
         </div>
-      </CardFooter>
+      </CardContent>
+      <CardFooter></CardFooter>
     </Card>
   )
 }
