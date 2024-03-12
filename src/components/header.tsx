245e3210faf1f52e082ef58e6847c0ade189482d
PikiLee <871565198@qq.com>
2024-03-12 12:46:38 +0800

    refactor: Add z-index to header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ import { H1 } from './ui/h1'
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 flex items-center justify-between w-full h-16 px-8 bg-white">
+    <header className="fixed top-0 left-0 flex items-center justify-between w-full h-16 px-8 bg-white z-50">
       <Link href="/">
         <H1>Image Bed Next</H1>
       </Link>
